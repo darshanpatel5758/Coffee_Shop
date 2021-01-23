41ddd221c48d2e86652ec47748ea0f993c51f4d7
darshanpatel5758 <darshanpatel5758@gmail.com>
2021-01-23 16:52:43 -0500

    Coffee

---
+++ Coffee_Shop/Controllers/Registration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coffee_Shop.Models.Registration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_Shop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public IActionResult FormView(RegistrationViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
